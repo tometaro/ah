9d4f5a4e46b2d2e39206244e29ba1da92a96cc02
TOMETARO <y.tometaro@gmail.com>
2017-06-29 02:03:33 +0900

    20170629-01

--- home.php
+++ home.php
@@ -27,6 +27,7 @@ get_header(); ?>
 
 						<?php endwhile; endif; ?>
 					</ul>
+					<a class="button" href="<?php echo get_category_link($category->cat_ID);?>">もっと見る</a>
 				</div>
 			<?php endforeach; ?>
 		</main><!-- #main -->
--- template-parts/articleList.php
+++ template-parts/articleList.php
@@ -2,8 +2,8 @@
   <?php if(has_post_thumbnail()): ?><!-- アイキャッチがある場合 -->
     <a href="<?php the_permalink(); ?>" class="articleList-image entry-image"><?php the_post_thumbnail( 'thumb300', array('style' => 'width:300px;height:180px;') ); ?></a>
   <?php else: ?><!-- アイキャッチがない場合 -->
-    <a href="<?php the_permalink(); ?>" class="entry-image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/noImage.svg" alt="NO IMAGE" title="NO IMAGE" style="width:300px;height:180px;" class="no-image list-no-image" /></a>
+    <a href="<?php the_permalink(); ?>" class="articleList-image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/noImage.svg" alt="NO IMAGE" title="NO IMAGE" style="width:300px;height:180px;" class="no-image list-no-image" /></a>
   <?php endif; ?>
-  <div class="post_right"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
-  <p class="time"><?php the_time('Y年m月d日（D）'); ?></p>
+  <div class="articleList-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+  <p class="articleList-time"><?php the_time('Y年m月d日（D）'); ?></p>
 </li>
