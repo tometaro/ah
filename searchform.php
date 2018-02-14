6c947c0dd4f841dddcfdcf40a2b1413f40bd6c8d
[tometaro] <[y.tometaro@gmail.com]>
2018-02-14 18:41:06 +0900

    CMT

---
+++ searchform.php
@@ -0,0 +1,4 @@
+  <form role="search" method="get" class="search" action="//localhost:3000/">
+  	<input type="search" class="search-field" placeholder="検索" value="" name="s">
+	  <button type="submit" class="search-submit" value="検索"></button>
+  </form>
--- template-parts/articleList.php
+++ template-parts/articleList.php
@@ -1,6 +1,9 @@
 <li>
   <a href="<?php the_permalink(); ?>">
-    <div class="articleList-image" style="background-image: url(<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>)"></div>
+    <div class="article-wrap">
+      <div class="articleList-image" style="background-image: url(<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>)"></div>
+      <div class="articleList-noImage"></div>
+    </div>
     <div class="articleList-title"><?php the_title(); ?></div>
     <p class="articleList-time"><?php echo get_post_time('M d, Y'); ?></p>
   </a>
