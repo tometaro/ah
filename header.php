adb29b44f0fd65e45953e2c816b60776edcc618b
留岡佑太郎 <>
2018-03-11 22:31:36 +0900

    こめ

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/sass/foundation/icon-font/dist/css/symbols.css" >
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/icon-font/dist/css/symbols.css" >
 
 <?php wp_head(); ?>
 </head>
